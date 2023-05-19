a7a35f2f72381148e4ecefedbe370febb181ef28
abhijitsaga <abhijitsagar1204@gmail.com>
2023-05-19 15:15:40 +0530

    change controller respone message

--- src/main/java/com/javaDevOps/cicd/controller/GreetingMessage.java
+++ src/main/java/com/javaDevOps/cicd/controller/GreetingMessage.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class GreetingMessage {
     @GetMapping("/")
     public String welcome(){
-        return "welcome to here......!";
+        return "welcome to India......!";
     }
 
     /*
